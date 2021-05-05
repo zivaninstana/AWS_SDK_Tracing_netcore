e4378cb3c7eaf3e573f1fbf8dee9304b37ec8bdb
zivaninstana <zivan.neskovic@instana.com>
2021-05-05 15:17:16 +0200

    Introducing custom SDK span to create send and receive correlation

--- Controllers/SQSController.cs
+++ Controllers/SQSController.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Amazon.SQS;
 using Amazon.SQS.Model;
+using Instana.Tracing.Sdk.Spans;
+using Instana.Tracing.Api;
+using System.Threading;
 
 namespace DynamoDBDemo.Controllers
 {
@@ -45,7 +48,7 @@ namespace DynamoDBDemo.Controllers
             {
                 //QueueUrl = "https://sqs.us-east-2.amazonaws.com/410797082306/dotnetTestQueue",
                 QueueUrl = "http://localhost:9324/queue/test",
-                MessageAttributes = new Dictionary<string, MessageAttributeValue>() { {"testKey", new MessageAttributeValue() { DataType = "String", StringValue = "testValue" } } },
+                //MessageAttributes = new Dictionary<string, MessageAttributeValue>() { {"testKey", new MessageAttributeValue() { DataType = "String", StringValue = "testValue" } } },
                 MessageBody = $"Test at {DateTime.UtcNow.ToLongDateString()}"
             };
 
@@ -82,25 +85,89 @@ namespace DynamoDBDemo.Controllers
         public async Task<ActionResult> ConsumeQueue()
         {
             ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest("http://localhost:9324/queue/test");
-            receiveMessageRequest.MessageAttributeNames = new List<string>(2) { "All" };
+            //receiveMessageRequest.MessageAttributeNames = new List<string>(2) { "X-INSTANA-T", "X-INSTANA-ST" };
             receiveMessageRequest.MaxNumberOfMessages = 10;
+
+            Message correlationMessage = null;
             ReceiveMessageResponse r = await _amazonSQS.ReceiveMessageAsync(receiveMessageRequest);
+            correlationMessage = r.Messages.FirstOrDefault();
+                List<DeleteMessageBatchRequestEntry> deleteReqEntries = new List<DeleteMessageBatchRequestEntry>(r.Messages.Count);
 
-            List<DeleteMessageBatchRequestEntry> deleteReqEntries = new List<DeleteMessageBatchRequestEntry>(r.Messages.Count);
-            foreach(var message in r.Messages)
+            using (var span = CustomSpan.Create()
+                       .AsAWSSQSMessageReceive(receiveMessageRequest.QueueUrl))
             {
-                Console.WriteLine("Messageid: " + message.MessageId);
-                Console.WriteLine("Message body:" + message.Body);
-                Console.WriteLine("Recepit: " + message.ReceiptHandle);
-                Console.WriteLine("MD5Body: " + message.MD5OfBody);
-                Console.WriteLine();
+                span.WrapAction(() =>
+                {
+                    foreach (var message in r.Messages)
+                    {
+                        Console.WriteLine("Messageid: " + message.MessageId);
+                        Console.WriteLine("Message body:" + message.Body);
+                        Console.WriteLine("Recepit: " + message.ReceiptHandle);
+                        Console.WriteLine("MD5Body: " + message.MD5OfBody);
+                        Console.WriteLine();
+                        Thread.Sleep(2000);
 
-                deleteReqEntries.Add(new DeleteMessageBatchRequestEntry(message.MessageId, message.ReceiptHandle));
+                        deleteReqEntries.Add(new DeleteMessageBatchRequestEntry(message.MessageId, message.ReceiptHandle));
+                    }
+
+
+                }, true);
+
+                span.AsChildOf(() => GetDisInfo(correlationMessage));
+
             }
-                DeleteMessageBatchRequest deleteMessageBatchRequest = new DeleteMessageBatchRequest("http://localhost:9324/queue/test", deleteReqEntries);
-                var response = await _amazonSQS.DeleteMessageBatchAsync(deleteMessageBatchRequest);
+            DeleteMessageBatchRequest deleteMessageBatchRequest = new DeleteMessageBatchRequest("http://localhost:9324/queue/test", deleteReqEntries);
+            var response = await _amazonSQS.DeleteMessageBatchAsync(deleteMessageBatchRequest);
 
             return Ok();
+        }
+
+        [HttpGet("odlconsume")]
+        public async Task<ActionResult> ConsumeQueueOld()
+        {
+            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest("http://localhost:9324/queue/test");
+            //receiveMessageRequest.MessageAttributeNames = new List<string>(2) { "X-INSTANA-T", "X-INSTANA-ST" };
+            receiveMessageRequest.MaxNumberOfMessages = 10;
+
+                    ReceiveMessageResponse r = _amazonSQS.ReceiveMessageAsync(receiveMessageRequest).Result;
+                    
+
+                    List<DeleteMessageBatchRequestEntry> deleteReqEntries = new List<DeleteMessageBatchRequestEntry>(r.Messages.Count);
+                    foreach (var message in r.Messages)
+                    {
+                        Console.WriteLine("Messageid: " + message.MessageId);
+                        Console.WriteLine("Message body:" + message.Body);
+                        Console.WriteLine("Recepit: " + message.ReceiptHandle);
+                        Console.WriteLine("MD5Body: " + message.MD5OfBody);
+                        Console.WriteLine();
+                        Thread.Sleep(2000);
+
+                        deleteReqEntries.Add(new DeleteMessageBatchRequestEntry(message.MessageId, message.ReceiptHandle));
+                    }
+
+                    DeleteMessageBatchRequest deleteMessageBatchRequest = new DeleteMessageBatchRequest("http://localhost:9324/queue/test", deleteReqEntries);
+                    var response = _amazonSQS.DeleteMessageBatchAsync(deleteMessageBatchRequest).Result;
+
+            return Ok();
+        }
+
+        public static DistributedTraceInformation GetDisInfo(Message msg)
+        {
+            DistributedTraceInformation disInfo = new DistributedTraceInformation();
+
+            if (msg == null) return disInfo;
+
+            if (msg.MessageAttributes.TryGetValue(TracingConstants.ExternalTraceIdHeader, out MessageAttributeValue traceIdAttributeValue))
+            {
+                disInfo.TraceId = TraceIdUtil.GetLongFromHex(traceIdAttributeValue.StringValue);
+            }
+
+            if (msg.MessageAttributes.TryGetValue(TracingConstants.ExternalParentSpanIdHeader, out MessageAttributeValue parentIdAttributeValue))
+            {
+                disInfo.ParentSpanId = TraceIdUtil.GetLongFromHex(parentIdAttributeValue.StringValue);
+            }
+
+            return disInfo;
         }
     }
 }
